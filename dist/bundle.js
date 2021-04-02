5b2b85e8d4f48fdfdebf63d314fbe2858bdacea8
Mateus Cazuza <mateus.cazuza@hotmail.com>
2021-04-02 07:31:07 -0300

    start bundler via babel

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _user$endereco;
+
+var user = {
+  name: "Mateus",
+};
+console.log(
+  (_user$endereco = user.endereco) === null || _user$endereco === void 0
+    ? void 0
+    : _user$endereco.rua
+);
